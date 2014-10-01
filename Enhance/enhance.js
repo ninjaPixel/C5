701072b2c8cf56ea18224e065fd2eb0a6a837f5d
ninjaPixel <matthew@ninjaPixel.eu>
2014-10-01 10:49:28 +0100

    informa on legend

--- Enhance/enhance.js
+++ Enhance/enhance.js
@@ -13,16 +13,16 @@ require.config({
         'queue': '../../js/queue.min',
         'XDate': '../../js/XDate',
         'jqueryUI': '../../js/jquery-ui-1.11.1/jquery-ui.min',
-        'dimple':'js/dimple.v2.1.0.min',
+        'dimple': 'js/dimple.v2.1.0.min',
         'enhanceCharts': 'js/enhanceCharts',
-        'enhanceData':'js/enhanceData'
+        'enhanceData': 'js/enhanceData'
     }
 });
 
 define([
     // Load our app module and pass it to our definition function
     'd3', 'enhanceCharts', 'enhanceData'
-], function (d3, enhanceCharts,enhanceData) {
+], function (d3, enhanceCharts, enhanceData) {
 
     var bubbleChart = d3.ninja.bubbleChart();
     var margin = {
@@ -31,46 +31,141 @@ define([
         left: 60,
         right: 20
     };
-    
+
     bubbleChart.height(400)
-    .width(600)
-    .border(false)
-    .maxBubbleRadius(25)
-    .yAxis1Title('Performance (previous 36 months)')
-    .xAxisTitle('Risk')
-    .title('Annualised Performance and Drawdown vs. Risk')
-    .strokeColour('#525252')
-    .bubbleSpill(false)
-    .yMin(-14)
-    .showGridLines(true)
-    .margin(margin);
+        .width(600)
+        .border(false)
+        .maxBubbleRadius(25)
+        .yAxis1Title('Performance (previous 36 months)')
+        .xAxisTitle('Risk')
+        .title('Annualised Performance and Drawdown vs. Risk')
+        .strokeColour('#525252')
+        .bubbleSpill(false)
+        .yMin(-14)
+        .showGridLines(true)
+        .margin(margin);
 
     var legend = d3.ninja.horizontalLegendSelectable();
-        var legendMargin = {
+    var legendMargin = {
         top: 20,
         bottom: 40,
         left: 10,
         right: 0
     };
     var legendItems = ['Cash Index', 'Low Risk', 'Medium Risk', 'High Risk', 'Equity Benchmark']
-    
 
-var bubbleChartData = getEnhanceDataXY();
+
+    var bubbleChartData = getEnhanceDataXY();
     console.log('enhance data', bubbleChartData);
-        d3.select('#bubbleChart')
-            .datum(bubbleChartData)
-            .call(bubbleChart);
-    
+    d3.select('#bubbleChart')
+        .datum(bubbleChartData)
+        .call(bubbleChart);
+
     legend.margin(legendMargin)
-    .width(250)
-    .itemWidth(250)
-    .fontColor('white')
-    .fontSize(20)
-    .fontColor('#333');
+        .width(250)
+        .itemWidth(250)
+        .fontColor('white')
+        .fontSize(20)
+        .fontColor('#333');
     d3.select('#legend')
         .datum(bubbleChartData.slice().reverse())
         .call(legend);
 
+
+
+
+    var drawInfoLegend = function () {
+
+
+        var svgContainer = d3.select('#infoLegend').append('svg')
+            .attr('width', 380)
+            .attr('height', 145);
+        // http://tributary.io/inlet/cc4700b1fb7c30177fa4
+        //var svgContainer = d3.select("svg");
+
+        var jsonCircles = [{
+                'x_axis': 22,
+                'y_axis': 22,
+                'radius': 20
+}],
+            fillColor = '#9b9b9b',
+            opacity = 0.7,
+            strokeColor = '#333';
+
+        var circles = svgContainer.selectAll('circle')
+            .data(jsonCircles)
+            .enter()
+            .append('circle');
+
+        circles.attr('cx', function (d) {
+            return d.x_axis;
+        })
+            .attr('cy', function (d) {
+                return d.y_axis;
+            })
+            .attr('r', function (d) {
+                return d.radius;
+            })
+            .style({
+                fill: fillColor,
+                opacity: opacity,
+                stroke: strokeColor
+            });
+
+        var text1 = svgContainer.selectAll('text1')
+            .data([0])
+            .enter()
+            .append('text').classed('text1', true);
+
+        text1.attr('x', 48)
+            .attr('y', 29)
+            .text('Annualised Performance over previous 36 months');
+
+
+        var x = 22,
+            y = 136;
+
+        svgContainer.append('line')
+            .attr({
+                x1: x,
+                y1: 50,
+                x2: x,
+                y2: y
+            })
+            .style({
+                stroke: fillColor,
+                'stroke-width': 3
+            });
+
+        svgContainer.append('line')
+            .attr({
+                x1: x - 10,
+                y1: y,
+                x2: x + 10,
+                y2: y
+            })
+            .style({
+                stroke: fillColor,
+                'stroke-width': 1
+            });
+
+        var text2 = svgContainer.selectAll('text2')
+            .data([0])
+            .enter()
+            .append('text').classed('text2', true);
+
+        text2.attr('x', 48)
+            .attr('y', 98)
+            .text('Max. drawdown over previous 36 months');
+
+
+
+
+
+
+
+
+    };
+
+    drawInfoLegend();
 });
-
-
