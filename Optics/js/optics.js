8e2d9a9c6519a7c9d017d29ad88e974d0906c58e
ninjaPixel <matthew@ninjaPixel.eu>
2014-10-03 15:03:34 +0100

    not much

--- Optics/js/optics.js
+++ Optics/js/optics.js
@@ -9,83 +9,31 @@ require.config({
         'moment-timezone': 'moment-timezone.v0.2.1.min',
         'moment-timezone-data': 'moment-timezone-with-data-2010-2020',
         'd3': 'd3.v3.4.11.min',
-        'queue': 'queue.min',    
-        'dimple':'dimple.v2.1.0.min'
+        'queue': 'queue.min',
+        'dimple': 'dimple.v2.1.0.min',
+        'ninjaCharts': 'ninjaCharts.v0.2.1',
+        'opticsData': 'opticsData'
     }
 });
 
 define([
     // Load our app module and pass it to our definition function
-    'd3'
-], function (d3) {
+    'd3', 'opticsData'
+], function (d3, opticsData) {
 
-    
-var n = 20, // number of layers
-    m = 200, // number of samples per layer
-    stack = d3.layout.stack().offset("wiggle"),
-    layers0 = stack(d3.range(n).map(function() { return bumpLayer(m); })),
-    layers1 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
 
-var width = 400,
-    height = 200;
+    var createTimeSeriesArray(instrumentName) {
+        var newTimeSeries = [];
+        opticsRawTimeSeries.forEach(function (d) {
+            newTimeSeries.push({
+                x: d.Dates,
+                y: d[instrumentName]
+            });
+        });
+        return [{
+            name: instrumentName,
+            values: newTimeSeries
+        }];
+    };
 
-var x = d3.scale.linear()
-    .domain([0, m - 1])
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .domain([0, d3.max(layers0.concat(layers1), function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); })])
-    .range([height, 0]);
-
-var color = d3.scale.linear()
-    .range(["#aad", "#556"]);
-
-var area = d3.svg.area()
-    .x(function(d) { return x(d.x); })
-    .y0(function(d) { return y(d.y0); })
-    .y1(function(d) { return y(d.y0 + d.y); });
-
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-svg.selectAll("path")
-    .data(layers0)
-  .enter().append("path")
-    .attr("d", area)
-    .style("fill", function() { return color(Math.random()); });
-
-function transition() {
-  d3.selectAll("path")
-      .data(function() {
-        var d = layers1;
-        layers1 = layers0;
-        return layers0 = d;
-      })
-    .transition()
-      .duration(2500)
-      .attr("d", area);
-}
-
-// Inspired by Lee Byron's test data generator.
-function bumpLayer(n) {
-
-  function bump(a) {
-    var x = 1 / (.1 + Math.random()),
-        y = 2 * Math.random() - .5,
-        z = 10 / (.1 + Math.random());
-    for (var i = 0; i < n; i++) {
-      var w = (i / n - y) * z;
-      a[i] += x * Math.exp(-w * w);
-    }
-  }
-
-  var a = [], i;
-  for (i = 0; i < n; ++i) a[i] = 0;
-  for (i = 0; i < 5; ++i) bump(a);
-  return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
-}
-    
 });
-
-
